19e33dac4f8ec0e589478b1ca330bd602654e478
SLUZ56 <99044741+SLUZ56@users.noreply.github.com>
2022-02-28 13:41:07 -0300

    I pushed my file
    I pushed my file

---
+++ mywork14.js
@@ -0,0 +1,6 @@
+// Faça um programa onde entraremos com dois números e ele imprime se o primeiro número é menor que o  segundo. Ex. 2 e 5 imprime True [ ].
+
+const firstNumber = 10
+const secondNumber = 15
+firstNumber < secondNumber
+console.log (firstNumber < secondNumber)
