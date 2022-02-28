f375efa994348a81a6afadc1c43a88055b9e48e2
SLUZ56 <99044741+SLUZ56@users.noreply.github.com>
2022-02-28 14:20:03 -0300

    I pushed my file
    I pushed my file

---
+++ mywork15.js
@@ -0,0 +1,7 @@
+/*Crie um programa que mostre o tamanho do array. Ex. const array = [ 0, 1] imprime:2.
+const array = ["banana", "Maçã", "laranja" ] imprime:3.*/
+
+const fruits = ['laranja', 'morango', 'abacaxi','uva']
+console.log(fruits.length)
+
+
