7ba989fe7c99e04ca44430a77613340be8d53256
SLUZ56 <99044741+SLUZ56@users.noreply.github.com>
2022-02-28 13:35:42 -0300

    I push my file
    I push my file

---
+++ mywork5.js
@@ -0,0 +1,10 @@
+//Faça um programa que imprima na tela se um nome é diferente ao outro digitado.Ex. João e João imprime True  ou João e Maria imprime False.
+
+const name1 = "Carlos"
+const name2 = "Marcos"
+const compare = name1 !== name2
+name1 !== name2
+
+console.log(compare)
+
+
