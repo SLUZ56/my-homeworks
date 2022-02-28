0517b7f8e67ae110e019063d4105297f6ca80405
SLUZ56 <99044741+SLUZ56@users.noreply.github.com>
2022-02-28 14:27:09 -0300

    I pushed my file
    I pushed my file

---
+++ homework9.js
@@ -0,0 +1,20 @@
+/*  Faça um programa onde ele leia 4 números. Em cada caso, ele deve retornar a mensagem
+ condizente: - Todos os números são impares => TODOS OS NÚMEROS SÃO IMPARES - 
+ Todos os números são pares => TODOS OS NÚMEROS SÃO PARES. Em caso negativo dos casos a cima,
+  colocar uma mensagem => Há números pares e ímpares.
+
+*/
+//ler 4 números
+let number1 = 2
+let number2 = 7 
+let number3 = 16
+let number4 = 23
+/* retornar mensagem dizendo 'Todos os números são ímpares.'
+ ou 'Todos os números são pares.' Ou caso negativo colocar 'Há números pares e ímpares.'*/
+ number1 = number1 +=2
+ number2 = number2 +=2
+ number3 = number3 +=2
+ number4 = number4 +=2
+let result = [number1, number2, number3, number4] 
+console.log(result)
+console.log('Há números PARES e ÍMPARES.')
