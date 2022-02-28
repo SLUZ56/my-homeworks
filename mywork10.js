c66734490584d1a126e175f9227506cf063702c9
SLUZ56 <99044741+SLUZ56@users.noreply.github.com>
2022-02-28 13:38:40 -0300

    I pushed my file
    I pushed my file

---
+++ mywork10.js
@@ -0,0 +1,10 @@
+/* Faça um programa que imprima na tela se um nome é igual ao outro digitado. Porém os 2 nomes devem estar em um objeto.
+ Ex const objeto = (firstName: "João ", secondName:"Maria")
+ João e João imprime True,  João e Maria imprime False.
+*/
+
+const names = {firstName: 'Carlos', secondName: 'Manuel'}
+const firstName = 'Carlos'
+const secondName = 'Manuel'
+
+console.log (firstName === secondName)
