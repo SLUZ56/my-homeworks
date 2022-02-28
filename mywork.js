ba62de8f0159254d3b9af020f27eb0b406da50cb
SLUZ56 <99044741+SLUZ56@users.noreply.github.com>
2022-02-28 14:27:43 -0300

    I pushed my file
    I pushed my file

---
+++ mywork.js
@@ -0,0 +1,15 @@
+ // 1- Faça um programa que some 2 números.
+
+ const myNumberOne = 56
+ const myNumberTwo = 38
+ const result = myNumberOne +
+ myNumberTwo
+ 
+ console.log (result)
+ 
+
+
+
+
+
+
