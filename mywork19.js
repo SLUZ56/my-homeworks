7d7792f97100aa09b50943aa18640e2d3172a46c
SLUZ56 <99044741+SLUZ56@users.noreply.github.com>
2022-02-28 14:22:00 -0300

    I pushed my file
    I pushed my file

---
+++ mywork19.js
@@ -0,0 +1,15 @@
+
+/*Estrutura de Repetição FOR
+i = index   toda vez que uso o for minha variável é i
+
+
+for(let i = 0; i < 10; i++ ){
+    console.log(i)
+}
+console.log('Terminou de executar meu For.')*/
+
+const students = ['Bruna', 'Ana', 'João', 'Maria', 'Pedro']
+
+for(let i = 0; i < students.length ; i++){
+    console.log (`${students[i]} está presente na aula.`)
+}    
