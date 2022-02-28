d64d62ec0c6ec83d77ecf836e40ea54ad9b8769b
SLUZ56 <99044741+SLUZ56@users.noreply.github.com>
2022-02-28 13:31:48 -0300

    I push a new file
    I push a new file

---
+++ homework1.js
@@ -0,0 +1,15 @@
+/* ] Faça um programa que compare um número com 10 e imprime na tela 
+"o número é maior, o número é menor o número é igual"
+
+*/
+ let x = 10
+if (x < 10){
+    console.log('O número é menor que 10.')
+} else 
+    if (x > 10){
+        console.log('O número é maior que 10.')
+    }
+    else {
+        console.log('O número é igual a 10.')
+    
+}
