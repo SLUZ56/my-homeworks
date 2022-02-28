6155d7f8e9246262d4930935b7d41838a6641582
SLUZ56 <99044741+SLUZ56@users.noreply.github.com>
2022-02-28 13:40:39 -0300

    I pushed my file
    I pushed my file

---
+++ mywork13.js
@@ -0,0 +1,5 @@
+// Faça um programa onde entramos com 2 números e ele imprime se o primeiro número é maior que o segundo. Ex. 2 e 5 . Imprime False [ ].
+const firstNumber = 10
+const secondNumber = 15
+firstNumber > secondNumber
+console.log (firstNumber > secondNumber)
