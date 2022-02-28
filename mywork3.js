ee3f92d6bcdc8fc65f47fe183d7c093440e02810
SLUZ56 <99044741+SLUZ56@users.noreply.github.com>
2022-02-28 13:34:30 -0300

    I push my file
    I push my file

---
+++ mywork3.js
@@ -0,0 +1,16 @@
+/* Faça um programa que inicie com 2 nomes, e o programa imprima na tela o seguinte dado:
+"Olá, meu nome é (nome1), e o meu partner do CodeClub é (nome2)." */
+
+const firstName = 'Selma'
+const secondName = 'Thaís'
+const names = `Olá, meu nome é ${firstName}, e o meu partner do CodeClub é ${secondName}.`
+
+console.log(names)
+
+
+
+
+
+
+
+
