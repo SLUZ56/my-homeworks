986e69a78eeade30622112235a77fe9bae5009b5
SLUZ56 <99044741+SLUZ56@users.noreply.github.com>
2022-02-28 13:36:13 -0300

    I push my file
    I push my file

---
+++ mywork6.js
@@ -0,0 +1,8 @@
+// Faça um programa que imprima na tela se um nome é diferente ao outro digitado. Porém os 2 nomes devem estar numa array. Ex. const array = ["João", "Maria"]. João e João imprime False. João e Maria imprime True.
+
+const names = ['Pablo', 'Pablo']
+const compare = names[0] !== names[1]
+
+console.log (compare)
+
+//
