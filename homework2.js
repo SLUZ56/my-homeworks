496d0468c13977c0ef19ee7c01800b713108c0a1
SLUZ56 <99044741+SLUZ56@users.noreply.github.com>
2022-02-28 14:23:40 -0300

    I pushed my file
    I pushed my file

---
+++ homework2.js
@@ -0,0 +1,10 @@
+/* Faça um programa que verifica se uma pessoa é brasileira ou não. // DEVE SER UM OBJETO
+
+*/
+const country = 'Argentina'
+if(country === 'Brasil') {
+    console.log('A pessoa é Brasileira.')
+} else {
+    console.log('A pessoa não é brasileira.')
+}
+console.log('Terminou a execução do código.')
