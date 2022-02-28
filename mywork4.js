c8a09fcf8be135bc2adcfa1968c8955d97bf9058
SLUZ56 <99044741+SLUZ56@users.noreply.github.com>
2022-02-28 13:35:09 -0300

    I push my file
    I push my file

---
+++ mywork4.js
@@ -0,0 +1,9 @@
+//Faça um programa que imprima na tela se um nome é igual ao outro digitado. Ex. João e João imprime True  ou João e Maria imprime False.
+
+let name1 = "Carlos"
+let name2 = "Miguel"
+const compare = name1 ===  name2
+
+console.log (compare)
+
+
