f0368d71a074ebcf783aa10d997b16606707a92a
SLUZ56 <99044741+SLUZ56@users.noreply.github.com>
2022-02-28 14:24:33 -0300

    I pushed my file
    I pushed my file

---
+++ homework4.js
@@ -0,0 +1,13 @@
+/* Faça um programa que receba 3 números inteiros, e deve imprimir,
+ qual é o maior, qual é o menor.
+
+*/
+
+const firstNumber = 24
+const secondNumber = 30
+const thirdNumber = 15
+if(secondNumber > firstNumber && secondNumber > thirdNumber)
+console.log('Esse número é o Maior.')
+if(thirdNumber < firstNumber && thirdNumber < secondNumber)
+console.log('Esse número é o Menor.')
+console.log('Terminou a execução do código.')
