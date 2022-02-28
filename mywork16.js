4c5c63d1308cd1e684f72678795a0106d21cea37
SLUZ56 <99044741+SLUZ56@users.noreply.github.com>
2022-02-28 14:20:33 -0300

    I pushed my file
    I pushed my file

---
+++ mywork16.js
@@ -0,0 +1,20 @@
+// Operadores Ternários ou Condicionais
+/* ? .... Se
+: .... Senão
+
+const rain = true
+const umbrela = rain ? 'Levar guarda-chuva.' : 'Deixar guarda-chuva.'
+console.log (umbrela)*/
+
+/* Preciso transferir meu dinheiro.
+
+const balance = true
+const isNotBlocket = true
+const accountExit = false
+
+const transferOk = balance && isNotBlocket  &&  accountExit ? 'TRANSFERÊNCIA REALIZADA' : "TRANSFERÊNCIA NEGADA"
+console.log (transferOk)*/
+
+const notRain = false
+const irAPraia = notRain ? 'Eu posso ir à praia hoje.' : 'Eu não irei à praia hoje.'
+console.log (irAPraia)
