e86306dfdd639e5b082ae8585799c5fe2e7cc1d6
SLUZ56 <99044741+SLUZ56@users.noreply.github.com>
2022-02-28 14:22:30 -0300

    I pushed my file
    I pushed my file

---
+++ while.js
@@ -0,0 +1,16 @@
+/* ESTRUTURA DE REPETIÇÃO - WHILE (ENQUANTO)
+WHILE só vai repetir enquanto a informação for verdadeira, se for falsa para de repetir.
+while(true){
+    console.log('algo')
+} 
+let i = 0
+while(i < 20){
+    console.log(i)
+    i +=2
+} */
+
+let i = 345267287934
+while(i > 20){
+    console.log(i)
+    i /= 50
+}
