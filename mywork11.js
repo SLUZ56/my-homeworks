f4c90f66e95e70b0fa7a528413d61e653eb3d6ab
SLUZ56 <99044741+SLUZ56@users.noreply.github.com>
2022-02-28 13:39:40 -0300

    I pushed my file
    I pushed my file

---
+++ mywork11.js
@@ -0,0 +1,5 @@
+// 11. Faça um programa que toda vez que rodar imprime um número aleatório entre 10 e 100.
+
+console.log (Math.floor (Math.random () * (100 - 10) + 10))//  aleatórios entre 0 e 1
+
+console.log (Math.random() * (90) + 10) // math.floor arredonda.
