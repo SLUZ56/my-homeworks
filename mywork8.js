cfc3d190a14dc663f0e6faf5ca3cfcf669818a17
SLUZ56 <99044741+SLUZ56@users.noreply.github.com>
2022-02-28 13:37:33 -0300

    I pushed my file
    I pushed my file

---
+++ mywork8.js
@@ -0,0 +1,34 @@
+//Crie 5 objetos,  neles deve conter os dados de 5 pessoas que você conhece. Mínimo 5 dados por pessoa.
+
+ const luiz = {
+    name: 'Luiz',
+    age: 25,
+    height: 1.80,
+    weight: 80,
+    sex: 'male'
+}
+const adriana = {
+    name:'Adriana',
+    age: 49,
+    height: 1.68,
+    weight: 58,
+    sex: 'famele'
+}
+
+const kelly = {
+     name: 'Kelly',
+     age: 39,
+     height: 1.78,
+     weight: 68,
+     sex: 'famele'
+}
+
+const norma = {
+     name: 'Norma',
+     age: 59,
+     height: 1.58,
+     weight: 48,
+     sex: 'famele'
+}
+
+
