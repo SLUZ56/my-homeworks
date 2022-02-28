99ec055e4494e45200d1536fc928c38a7c978881
SLUZ56 <99044741+SLUZ56@users.noreply.github.com>
2022-02-28 14:21:31 -0300

    I pushed my file
    I pushed my file

---
+++ mywork18.js
@@ -0,0 +1,32 @@
+/* Controle de Fluxo - Condicionais SWITH CASE
+
+const tempeture = 33
+switch (tempeture) {
+    case 30:
+        console.log ('A pessoa está com a temperatura Muito abaixo do normal.')
+        
+        break;
+    case 33:
+        console.log ('A pessoa está com a temperatura abaixo do normal.')
+
+        break;
+    case 36:
+        console.log ('A pessoa está com a temperatura normal.')
+
+        break;    
+
+    default:
+        console.log ('Temperatura não encontrada.')     
+        break;
+} */
+const bool = true
+
+switch (bool) {
+    case true:
+        console.log ("It's true.")
+        break;
+        case false:
+            console.log ("It's false.")
+            break;
+        
+}
