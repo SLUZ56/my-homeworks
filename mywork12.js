fdd98c998d649521b5c5237367add607506798eb
SLUZ56 <99044741+SLUZ56@users.noreply.github.com>
2022-02-28 13:40:11 -0300

    I pushed my file
    I pushed my file

---
+++ mywork12.js
@@ -0,0 +1,11 @@
+// .  Faça um programa onde colocamos 2 números ímpares e o programa imprima o resto da divisão. Ex. 7/3 deve imprimir:1. 25/5 deve imprimir:0. [ ]
+
+/*const firstNumber = 11
+const secondNamber = 9
+firstNumber % secondNamber
+console.log (firstNumber % secondNamber)*/
+
+const firstNumber = 25
+const secondNamber = 13
+firstNumber % secondNamber
+console.log (firstNumber % secondNamber)
